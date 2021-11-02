540d5018e15393ae8506867809761520bd4b2d3d
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2021-11-01 20:59:11 -0400

    Added video formatter class.

---
+++ src/Plugin/Field/Formatter/IbmVideoFormatter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Drupal\ibm_video_media_type\Plugin\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Formats the IBM video media type source field.
+ *
+ * @FieldFormatter(
+ *   id = "ibm_video",
+ *   label = @Translation("IBM video"),
+ *   field_types = {"string_long"}
+ * )
+ */
+class IbmVideoFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'useAutoplay' => FALSE,
+      'useHtml5Ui' => TRUE,
+      'displayControls' => TRUE,
+      'initialVolume' => 50,
+      'showTitle' => TRUE,
+      'wMode' => NULL,
+      'defaultQuality' => NULL,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    // @todo Finish.
+    throw new \RuntimeException('Method not implemented.');
+  }
+
+}
