16c098c38b03f6c02a7799ce7185a03d41033687
duncan-mcclenagan <duncan_mcclenagan@ucg.org>
2021-11-10 15:55:29 -0500

    Cleanup, bug fix.

--- src/Plugin/media/Source/IbmVideo.php
+++ src/Plugin/media/Source/IbmVideo.php
@@ -130,87 +130,8 @@ class IbmVideo extends MediaSourceBase implements MediaSourceFieldConstraintsInt
    */
   public function getSourceFieldConstraints() : array {
     return [
-      'ibm_video_configuration' => [],
+      'ibm_video_data' => [],
     ];
-  }
-
-  /**
-   * Parses data from the source field into an array.
-   *
-   * @param string $data
-   *   Data from the first item of the source field.
-   * @param array<string, mixed> $parsedData
-   *   (output parameter) If parsing was successful, this is an array consisting
-   *   of two items: one with key static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME
-   *   containing the channel ID (the channel ID is not validated, though), and
-   *   another with key static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME containing
-   *   the channel video ID (which is also not validated).
-   *
-   * @return int
-   *   Returns 0 on success. Otherwise, returns either
-   *   static::VIDEO_DATA_PARSE_ERROR_BAD_JSON (for malformed JSON), or
-   *   static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS (for an incorrect set of JSON
-   *   keys in the root JSON element).
-   */
-  public function tryParseVideoData(string $data, array &$parsedData) : int {
-    $parsedData = json_decode($data, TRUE);
-    if (is_array($parsedData)) {
-      if (count($parsedData) !== 2) {
-        $parsedData = [];
-        return static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS;
-      }
-      if (!array_key_exists(static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME, $parsedData) || !array_key_exists(static::VIDEO_DATA_CHANNEL_VIDEO_ID_PROPERTY_NAME, $parsedData)) {
-        $parsedData = [];
-        return static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS;
-      }
-    }
-    else {
-      $parsedData = [];
-      return static::VIDEO_DATA_PARSE_ERROR_BAD_JSON;
-    }
-
-    return 0;
-  }
-
-  /**
-   * Validates and retrieves, from the parsed video data, the channel ID.
-   *
-   * @param array $parsedVideoData
-   *   Parsed video data (from tryParseVideoData()).
-   * @param string $channelId
-   *   (output parameter) If validation was successful, the channel ID.
-   *
-   * @return bool
-   *   Returns TRUE if the channel ID was valid; else returns FALSE.
-   */
-  public function tryGetChannelId(array $parsedVideoData, string &$channelId) : bool {
-    $channelId = $parsedVideoData[static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME];
-    if (!is_string($channelId)) {
-      $channelId = '';
-      return FALSE;
-    }
-    return ValidationHelpers::isChannelIdValid($channelId);
-  }
-
-  /**
-   * Validates and retrieves, from the parsed video data, the channel video ID.
-   *
-   * @param array $parsedVideoData
-   *   Parsed video data (from tryParseVideoData()).
-   * @param string $channelVideoId
-   *   (output parameter) If validation was successful, the channel video ID.
-   *
-   * @return bool
-   *   Returns TRUE if the channel video ID was valid; else returns FALSE.
-   */
-  public function tryGetChannelVideoId(array $parsedVideoData, string &$channelVideoId) : bool {
-    $channelVideoId = $parsedVideoData[static::VIDEO_DATA_CHANNEL_VIDEO_ID_PROPERTY_NAME];
-    if (!is_string($channelVideoId)) {
-      $channelVideoId = '';
-      return FALSE;
-    }
-
-    return ValidationHelpers::isChannelVideoIdValid($channelVideoId);
   }
 
   /**
@@ -269,24 +190,91 @@ class IbmVideo extends MediaSourceBase implements MediaSourceFieldConstraintsInt
   }
 
   /**
+   * Validates and retrieves, from the parsed video data, the channel ID.
+   *
+   * @param array $parsedVideoData
+   *   Parsed video data (from tryParseVideoData()).
+   * @param string $channelId
+   *   (output parameter) If validation was successful, the channel ID.
+   *
+   * @return bool
+   *   Returns TRUE if the channel ID was valid; else returns FALSE.
+   */
+  public function tryGetChannelId(array $parsedVideoData, string &$channelId) : bool {
+    $channelId = $parsedVideoData[static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME];
+    if (!is_string($channelId)) {
+      $channelId = '';
+      return FALSE;
+    }
+    return ValidationHelpers::isChannelIdValid($channelId);
+  }
+
+  /**
+   * Validates and retrieves, from the parsed video data, the channel video ID.
+   *
+   * @param array $parsedVideoData
+   *   Parsed video data (from tryParseVideoData()).
+   * @param string $channelVideoId
+   *   (output parameter) If validation was successful, the channel video ID.
+   *
+   * @return bool
+   *   Returns TRUE if the channel video ID was valid; else returns FALSE.
+   */
+  public function tryGetChannelVideoId(array $parsedVideoData, string &$channelVideoId) : bool {
+    $channelVideoId = $parsedVideoData[static::VIDEO_DATA_CHANNEL_VIDEO_ID_PROPERTY_NAME];
+    if (!is_string($channelVideoId)) {
+      $channelVideoId = '';
+      return FALSE;
+    }
+
+    return ValidationHelpers::isChannelVideoIdValid($channelVideoId);
+  }
+
+  /**
+   * Parses data from the source field into an array.
+   *
+   * @param string $data
+   *   Data from the first item of the source field.
+   * @param array<string, mixed> $parsedData
+   *   (output parameter) If parsing was successful, this is an array consisting
+   *   of two items: one with key static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME
+   *   containing the channel ID (the channel ID is not validated, though), and
+   *   another with key static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME containing
+   *   the channel video ID (which is also not validated).
+   *
+   * @return int
+   *   Returns 0 on success. Otherwise, returns either
+   *   static::VIDEO_DATA_PARSE_ERROR_BAD_JSON (for malformed JSON), or
+   *   static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS (for an incorrect set of JSON
+   *   keys in the root JSON element).
+   */
+  public function tryParseVideoData(string $data, array &$parsedData) : int {
+    $parsedData = json_decode($data, TRUE);
+    if (is_array($parsedData)) {
+      if (count($parsedData) !== 2) {
+        $parsedData = [];
+        return static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS;
+      }
+      if (!array_key_exists(static::VIDEO_DATA_CHANNEL_ID_PROPERTY_NAME, $parsedData) || !array_key_exists(static::VIDEO_DATA_CHANNEL_VIDEO_ID_PROPERTY_NAME, $parsedData)) {
+        $parsedData = [];
+        return static::VIDEO_DATA_PARSE_ERROR_INVALID_KEYS;
+      }
+    }
+    else {
+      $parsedData = [];
+      return static::VIDEO_DATA_PARSE_ERROR_BAD_JSON;
+    }
+
+    return 0;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) : IbmVideo {
     // We call parent::create() instead of invoking our own constructor, because
     // Drupal plugin constructors are technically not part of the public API.
     return parent::create($container, $configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * Throws an exception saying the source field of a media entity is invalid.
-   *
-   * @param string $entityVariableName
-   *   Media entity variable name (to use in the exception).
-   *
-   * @throws \InvalidArgumentException
-   */
-  private static function throwSourceFieldInvalidException(string $entityVariableName) : void {
-    throw new \InvalidArgumentException('The source field for $' . $entityVariableName . ' is corrupt.');
   }
 
 }
